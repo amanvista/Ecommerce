4453c945e52b9b3e823061c55da49653518bf3a0
Aman Bisht <amanvista@gmail.com>
2022-01-17 15:24:11 +0530

    logout working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,3 +37,14 @@ exports.loginUser = catchAsyncErrors (
     }
 )
 
+// Logout User
+exports.logout = catchAsyncErrors( async (req,res,next)=>{
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    })
+}) 
