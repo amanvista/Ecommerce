f49a79d34d8501e87e641fffa9a79bfc8993bb23
Aman Bisht <amanvista@gmail.com>
2022-01-17 19:56:13 +0530

    product added by added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,7 @@ const ApiFeatures = require("../utils/apiFeatures")
 //create Product - Admin route
 exports.createProduct = catchAsyncErrors(
 async ( req, res, next)=>{
+    req.body.user = req.user.id
     const product = await Product.create(req.body)
     res.status(201).json({
         success: true,
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,7 +3,7 @@ const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductD
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth")
 const router = express.Router()
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts)
-router.route("/product/new").post(isAuthenticatedUser,createProduct)
+router.route("/products").get(getAllProducts)
+router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct)
 router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 module.exports = router
